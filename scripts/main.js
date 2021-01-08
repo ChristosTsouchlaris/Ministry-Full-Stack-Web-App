24976227b2e7e06fd9e97d2e6418a6843d384770
ChristosTsouchlaris <75982015+ChristosTsouchlaris@users.noreply.github.com>
2021-01-08 09:53:00 +0200

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+const infoButton = document.querySelector("#btn-info");
+const employerButton = document.querySelector("#btn-employer");
+const employeeButton = document.querySelector("#btn-employee");
+
+
+infoButton.addEventListener("click", (e) => {
+    window.location.href = "/information/info.html";
+})
+
+employerButton.addEventListener("click", (e) => {
+    window.location.href = "/employer/employer.html";
+})
+
+infoButton.addEventListener("click", (e) => {
+    window.location.href = "/information/info.html";
+})
