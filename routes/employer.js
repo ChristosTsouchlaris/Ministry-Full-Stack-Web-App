4746a078bf24ae0f899f0c53ecf6d644ad848ff8
mereosk <mereosk@yahoo.gr>
2021-01-19 04:54:19 +0200

    added the 'employer can't add same employee' functionality

--- routes/employer.js
+++ routes/employer.js
@@ -10,25 +10,35 @@ router.get('/employer_profile/:id' , (req, res) => {
     res.render('employerProfile/employer');
 })
 
-router.get('/employer_profile/:id/employer_staff' , (req, res) => {
-    res.render('employerProfile/employer-staff');
+router.get('/employer_profile/:id/employer_staff' , async (req, res) => {
+    const employer = await User.findById(req.params.id).populate('employees');
+    console.log(employer)
+    res.render('employerProfile/employer-staff', { employer });
 })
 
 router.post('/employer/:id/new_employee', async (req, res, next) => {
     const { amka } = req.body;
     // Find the employer with the id from the url
-    const employer = await User.findById(req.params.id);
+    const employer = await User.findById(req.params.id).populate('employees');
+    // console.log(employer.employees["_id"]);
     // Find the employee from the given amka
     const employee = await User.findOne({amka: amka});
+
+    // Check if this employee already works for this employer
+    if (employer.employees.filter(function(e) { return e.amka == employee.amka; }).length > 0) {
+        req.flash('error', "Αυτός ο υπάλληλος δουλεύει ήδη για εσάς!");
+        res.redirect(`/employer_profile/${employer._id}/employer_staff`)
+    }
+
     if(!employee) {
-        req.flash('error', "There doesn't exist an employer with that AMKA. Try again!");
-        res.redirect('/employer_profile/employer_staff')
+        req.flash('error', "Δεν υπάρχει υπάλληλος με αυτό το ΑΜΚΑ");
+        res.redirect(`/employer_profile/${employer._id}/employer_staff`)
     }
     else {
         employer.employees.push(employee);
         await employer.save();
-        req.flash('success', "Good job, you inserted an employee!")
-        res.redirect('/employer_profile/employer_staff')
+        req.flash('success', "Μπράβο εισήγαγες ένα υπάλληλο!")
+        res.redirect(`/employer_profile/${employer._id}/employer_staff`)
     }
 });
 
